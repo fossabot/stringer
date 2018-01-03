c0fe7696f60363d96bca1839b0c5af8226c0d18f
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-04 00:04:47 +0200

    Add user spec

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Feed < ApplicationRecord
   has_many :stories, -> { order "published desc" }, dependent: :delete_all
   belongs_to :group
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
 
+  # DEPRECATED: remove this
   def setup_complete?
     setup_complete
   end
