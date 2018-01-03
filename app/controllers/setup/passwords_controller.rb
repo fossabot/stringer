450cc250783e174d5e63860c9cb0a0bedea2ef0a
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-03 13:16:20 +0200

    Rollback url changes

---
+++ app/controllers/setup/passwords_controller.rb
@@ -0,0 +1,35 @@
+module Setup
+  class PasswordsController < ApplicationController
+    before_action :setup_complete
+
+    def show
+    end
+
+    def create
+      if no_password(params) || password_mismatch?(params)
+        flash.now[:error] = t('first_run.password.flash.passwords_dont_match')
+
+        render :show
+      else
+        user = CreateUser.new.create(params[:password])
+        session[:user_id] = user.id
+
+        redirect_to '/feeds/import'
+      end
+    end
+
+    private
+
+    def setup_complete
+      redirect_to '/news' if UserRepository.setup_complete?
+    end
+
+    def no_password(params)
+      params[:password].nil? || params[:password] == ""
+    end
+
+    def password_mismatch?(params)
+      params[:password] != params[:password_confirmation]
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resource :setup_password, only: [:show, :create]
+  namespace :setup do
+    resource :password, only: [:show, :create]
+  end
 
   resource :session, only: [:new, :create, :destroy]
 
