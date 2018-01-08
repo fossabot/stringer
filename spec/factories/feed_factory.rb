ffd98fc3188a5e2ad9c950cf3e652049580f9af4
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-09 00:40:45 +0200

    Update feed factory

--- spec/factories/feed_factory.rb
+++ spec/factories/feed_factory.rb
@@ -1,27 +1,10 @@
-class FeedFactory
-  class FakeFeed < OpenStruct
-    def as_fever_json
-      {
-        id: id,
-        favicon_id: 0,
-        title: name,
-        url: url,
-        site_url: url,
-        is_spark: 0,
-        last_updated_on_time: last_fetched.to_i
-      }
-    end
-  end
+# frozen_string_literal: true
 
-  def self.build(params = {})
-    FakeFeed.new(
-      id: rand(100),
-      group_id: params[:group_id] || rand(100),
-      name: params[:name] || Faker::Name.name + " on Software",
-      url: params[:url] || Faker::Internet.url,
-      last_fetched: params[:last_fetched] || Time.now,
-      stories: params[:stories] || [],
-      unread_stories: []
-    )
+FactoryBot.define do
+  factory :feed do
+    group
+    name { "#{ Faker::Name.name } on Software" }
+    url { Faker::Internet.url }
+    last_fetched { Time.now }
   end
 end
