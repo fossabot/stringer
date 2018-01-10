3b85ca658163b7a770f8284c82f5b5850cec0dc7
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-10 16:06:30 +0200

    Add config/credentials.yml.enc for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :branch, 'port-to-rails'
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 
-set :linked_files, ['config/database.yml', 'config/newrelic.yml', '.env']
+set :linked_files, ['config/database.yml', 'config/credentials.yml.enc', 'config/newrelic.yml', '.env']
 set :linked_dirs, ['bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/assets']
 set :config_files, ['database.yml']
 
