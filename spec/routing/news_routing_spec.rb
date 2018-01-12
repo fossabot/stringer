0f0c809536cf4b61abb572fd99d2799e68cef83a
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-12 17:21:13 +0200

    Remove useless home controller

--- spec/routing/news_routing_spec.rb
+++ spec/routing/news_routing_spec.rb
@@ -4,6 +4,13 @@ require 'rails_helper'
 
 describe NewsController do
   describe 'routing' do
+    it 'should route GET / to news#index' do
+      expect(get: '/').to route_to(
+        controller: 'news',
+        action: 'index'
+      )
+    end
+
     it 'should route GET /news to news#index' do
       expect(get: '/news').to route_to(
         controller: 'news',
