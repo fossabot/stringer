24f14d468a6b0c43141699e6b37fa97f086c61bd
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-12 23:09:48 +0200

    Fix rails console running in production server

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,12 +10,15 @@ set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 
 set :linked_files, ['config/database.yml', 'config/credentials.yml.enc', 'config/newrelic.yml', '.env']
-set :linked_dirs, ['bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/assets']
+set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/assets']
 set :config_files, ['database.yml']
 
 set :deploy_to, '/home/reader/www'
 
 set :keep_releases, 2
+
+# disable bin/ files overriding during deploy
+set :bundle_binstubs, nil
 
 namespace :deploy do
   after :restart, :clear_cache do
