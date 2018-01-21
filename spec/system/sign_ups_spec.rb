a2b2e86c60fc9957aa502ca2a12588356c1fb346
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-21 23:20:23 +0200

    One more system rspec example

--- spec/system/sign_ups_spec.rb
+++ spec/system/sign_ups_spec.rb
@@ -14,6 +14,35 @@ describe 'Sign ups' do
     expect(current_path).to eq(news_index_path)
   end
 
-  it 'when user try use already used email' do
+  it 'when user try to sign up with already used email' do
+    create(:user, email: 'me@example.com')
+
+    visit new_sign_up_path
+
+    expect {
+      fill_in 'sign_up[email]', with: 'me@example.com'
+      fill_in 'sign_up[password]', with: 'password'
+      fill_in 'sign_up[password_confirmation]', with: 'password'
+      click_button 'Sign Up'
+    }.not_to change(User, :count)
+
+    expect(current_path).to eq(sign_up_path)
+
+    expect(page).to have_content('Email has already been taken')
+  end
+
+  it 'when user try to sign ups with wrong password confirmation' do
+    visit new_sign_up_path
+
+    expect {
+      fill_in 'sign_up[email]', with: 'me@example.com'
+      fill_in 'sign_up[password]', with: 'password'
+      fill_in 'sign_up[password_confirmation]', with: 'wrong-confirmation'
+      click_button 'Sign Up'
+    }.not_to change(User, :count)
+
+    expect(current_path).to eq(sign_up_path)
+
+    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 end
