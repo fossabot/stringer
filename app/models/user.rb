86a5b1123076ddc3b066f8a045f34760105afef7
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-21 23:44:03 +0200

    Move validations to form object

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,5 @@ class User < ApplicationRecord
 
   has_many :stories, through: :feeds
 
-  # validates :email, presence: true
-  #
-  # validates :email, uniqueness: { case_sensitive: false }
-
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,9 +9,5 @@ describe User do
 
   it { should have_many(:stories).through(:feeds) }
 
-  # it { should validate_presence_of(:email) }
-  #
-  # it { should validate_uniqueness_of(:email).case_insensitive }
-
   it { should have_secure_password }
 end
