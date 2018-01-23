95dc31bf10c0b081b60fc011fab8418ab8f875ef
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-24 01:12:11 +0200

    Export current_user feeds

--- app/controllers/feeds/exports_controller.rb
+++ app/controllers/feeds/exports_controller.rb
@@ -3,7 +3,7 @@
 module Feeds
   class ExportsController < ApplicationController
     def show
-      send_data(ExportToOpml.new(Feed.select(:name, :url)).to_xml,
+      send_data(::ExportToOpml.new(current_user.feeds.select(:name, :url)).to_xml,
                 disposition: 'attachment',
                 type: 'application/xml',
                 filename: 'stringer.opml')
