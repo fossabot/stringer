6fbd1a1711daf2cbd92ffe3592e43ff65e0f51a7
Igor Zubkov <igor.zubkov@gmail.com>
2018-01-24 01:29:14 +0200

    Refactoring

--- app/commands/feeds/add_new_feed.rb
+++ app/commands/feeds/add_new_feed.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 class AddNewFeed
-  ONE_DAY = 24 * 60 * 60
-
-  def self.add(current_user, url, discoverer = FeedDiscovery.new, repo = Feed)
+  def self.add(current_user, url, discoverer = FeedDiscovery.new)
     result = discoverer.discover(url)
     return false unless result
 
-    repo.create(user: current_user,
-                name: result.title,
-                url: result.feed_url,
-                last_fetched: Time.now - ONE_DAY)
+    current_user.feeds.create!(name: result.title,
+                               url: result.feed_url,
+                               last_fetched: 1.day.ago)
   end
 end
